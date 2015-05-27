92079ca3253e1b08d8b0a9db1a10bb1127263bf8
Magnus Skjegstad <magnus@skjegstad.com>
2015-05-28 00:18:48 +0200

    Update/add copyrights

--- src/synjitsu.mli
+++ src/synjitsu.mli
@@ -1,5 +1,5 @@
 (*
- * Copyright (c) 2014 Magnus Skjegstad <magnus@skjegstad.com>
+ * Copyright (c) 2015 Magnus Skjegstad <magnus@skjegstad.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
